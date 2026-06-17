6477657c0acd9e0f34c9aeca810c6a4df4b50995
Dmitry Komarov <dimon.on.on.23.02@gmail.com>
2026-06-18 00:20:23 +0600

    day 5 small dict work

---
+++ day5.py
@@ -0,0 +1,43 @@
+print('#1')
+
+country = dict(code='RU', name='Russia', population=143000000)
+
+print(country.keys())
+print(country.items())
+print(country.values())
+
+print('#2')
+text = 'hello world'
+alphabet = ['a', 'b', 'c', 'd','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+probability = {}
+counterOutside = 0
+counterInside = 0
+counter = 0
+lenText = len(text)
+
+for char in alphabet:
+    counter = 0
+    for sign in text:
+        if char == sign:
+            counter+=1
+            counterInside+=1
+    if counter>0:
+        probability[char] = counter
+print(probability)
+
+print('#3')
+
+firstDict = {'a':20, 'b':40, 'c':41}
+secondDict = {'b':9, 'e':35, 'h':66}
+result = {}
+
+for key in firstDict:
+    for sKey in secondDict:
+        result[key]=firstDict.get(key, 0)+secondDict.get(key, 0)
+        if key!=sKey:
+            result[sKey]=firstDict.get(sKey, 0)+secondDict.get(sKey, 0)
+
+print(result)
+
+
